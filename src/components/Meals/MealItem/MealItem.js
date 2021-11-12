2fa0231f0f27007d93a0fcda0d8454074f216c44
anastazjasta <aneta.dakalo@gmail.com>
2021-11-12 19:22:25 +0100

    feat: form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,8 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`;
-
+  const price = `$${props.price.toFixed(2)}`;
 
   return (
     <li className={classes.meal}>
@@ -13,7 +13,7 @@ const MealItem = (props) => {
       </div>
       <div>
         <div>
-
+          <MealItemForm id={props.id} />
         </div>
       </div>
     </li>
